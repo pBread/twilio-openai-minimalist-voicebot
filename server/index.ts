6e0fa81e4298eab6e33a2a1b84c23927e6f0c011
pBread <4473570+pBread@users.noreply.github.com>
2024-10-02 12:22:12 -0600

    added msg handler boilerplate

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv-flow";
 import express from "express";
 import ExpressWs from "express-ws";
+import type { TwilioStreamMessage } from "./types";
 
 dotenv.config();
 
@@ -20,7 +21,7 @@ app.post("/incoming-call", async (req, res) => {
   res.end(`
       <Response>
         <Connect>
-          <Stream url="wss://${process.env.HOSTNAME}/connection/${CallSid}"
+          <Stream url="wss://${process.env.HOSTNAME}/media-stream/${CallSid}"
         </Connect>
       </Response>
       `);
@@ -36,13 +37,28 @@ app.post("/call-status-update", (req, res) => {
 /****************************************************
  Media Stream
 ****************************************************/
-app.ws("/connection/:callSid", (ws, req) => {
+app.ws("/media-stream/:callSid", (ws, req) => {
   const CallSid = req.params.callSid;
   console.log(`establishing websocket ${CallSid}`);
 
   ws.on("error", (err) => console.error(`websocket error`, err));
 
-  ws.on("message", (data) => {});
+  ws.on("message", (data) => {
+    let msg: TwilioStreamMessage;
+    try {
+      msg = JSON.parse(data.toString());
+    } catch (error) {
+      console.error("unexpected websocket message datatype");
+      return;
+    }
+
+    if (msg.event === "connected") console.log("websocket connected");
+    else if (msg.event === "mark") {
+    } else if (msg.event === "start") console.log("media stream started");
+    else if (msg.event === "stop") console.log("media stream stopped");
+    else if (msg.event === "media") {
+    } else console.warn(`unhandled media stream message`, msg);
+  });
 });
 
 /****************************************************
