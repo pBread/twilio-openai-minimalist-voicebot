d44502a606ccef1e0b60cd873b626a795f56aaf3
pBread <4473570+pBread@users.noreply.github.com>
2024-10-02 12:14:00 -0600

    added ws route boiler

--- server/index.ts
+++ server/index.ts
@@ -10,14 +10,40 @@ app.use(express.urlencoded({ extended: true })).use(express.json());
 /****************************************************
  Webhook Endpoints
 ****************************************************/
-app.post("/incoming-call", async (req, res) => {});
+app.post("/incoming-call", async (req, res) => {
+  const { CallSid, From, To } = req.body;
+  console.log(`incoming-call from ${From} to ${To}`);
 
-app.post("/call-status-update", (req, res) => {});
+  res.status(200);
+  res.type("text/xml");
+
+  res.end(`
+      <Response>
+        <Connect>
+          <Stream url="wss://${process.env.HOSTNAME}/connection/${CallSid}"
+        </Connect>
+      </Response>
+      `);
+});
+
+app.post("/call-status-update", (req, res) => {
+  const { CallSid, CallStatus } = req.body;
+  console.log(`call-status-update ${CallSid} ${CallStatus}`);
+
+  res.status(200).send();
+});
 
 /****************************************************
  Media Stream
 ****************************************************/
-app.ws("/connection/:callSid", (ws, req) => {});
+app.ws("/connection/:callSid", (ws, req) => {
+  const CallSid = req.params.callSid;
+  console.log(`establishing websocket ${CallSid}`);
+
+  ws.on("error", (err) => console.error(`websocket error`, err));
+
+  ws.on("message", (data) => {});
+});
 
 /****************************************************
  Start Server
