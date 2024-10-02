19a8ce267901149bac6096c28af528a30f51ac57
pBread <4473570+pBread@users.noreply.github.com>
2024-10-02 16:34:09 -0600

    converted const to fn

--- lib/twilio.ts
+++ lib/twilio.ts
@@ -2,16 +2,27 @@ import { WebSocket } from "ws";
 import { TwilioStreamAction } from "./twilio-types";
 
 let streamSid: string;
-export const setStreamSid = (sid: string) => (streamSid = sid);
+export function setStreamSid(sid: string) {
+  streamSid = sid;
+}
 
 let ws: WebSocket;
-export const setWs = (wss: WebSocket) => (ws = wss);
+export function setWs(wss: WebSocket) {
+  ws = wss;
+}
 
-export const dispatch = (event: TwilioStreamAction) =>
+/****************************************************
+ Media Stream Actions
+****************************************************/
+export function dispatch(event: TwilioStreamAction) {
   ws?.send(JSON.stringify(event));
-
-export const clearAudio = () => dispatch({ event: "clear", streamSid });
-export const sendAudio = (audio: string) =>
+}
+export function clearAudio() {
+  dispatch({ event: "clear", streamSid });
+}
+export function sendAudio(audio: string) {
   dispatch({ event: "media", streamSid, media: { payload: audio } });
-export const sendMark = (name: string) =>
+}
+export function sendMark(name: string) {
   dispatch({ event: "mark", streamSid, mark: { name } });
+}
