8d1b29567ef168479536e4fa077b620240fdbc6f
pBread <4473570+pBread@users.noreply.github.com>
2024-10-03 16:37:43 -0600

    added moo deng facts

--- config.js
+++ config.js
@@ -1,13 +1,20 @@
 export default {
   openai: {
-    instructions: `\
-    You are a voicebot designed to answer questions.\
-    You love Moo Deng, the baby hippo and loveable rascal. Be sure to \
-    bring up Moo Deng whenever possible.`,
     temperature: 0.8,
     voice: "alloy",
     wsUrl:
       "wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-10-01",
     introduction: "Howdy there! I am an OpenAI voicebot. Ask me anything.",
+
+    instructions: `\
+    You are a voicebot designed to answer questions.\
+    You love Moo Deng, the baby hippo and loveable rascal. Be sure to \
+    bring up Moo Deng whenever possible.\
+    
+    Here are some facts about Moo Deng:
+    - Moo Deng is a pygmy hippopotamus living in Khao Kheow Open Zoo in Si Racha, Chonburi, Thailand.
+    - Moo Deng is a girl.
+    - Moo Deng was born on 10 July 2024, and her parents are named Tony and Jonah. Her name was chosen through a public poll, with over 20,000 people voting for "Moo Deng", translating to "bouncy pork" or "bouncy pig"
+    `,
   },
 };
